3e5d4cf5d7e17207a4a5d279eaaa3ee5c2a368c1
Austin Brown <38289504+austinbrown22@users.noreply.github.com>
2018-10-13 10:19:52 -0500

    Add files via upload

---
+++ Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Austin Brown on 10/13/18.
+//  Copyright © 2018 Austin Brown. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    
+    let name: String
+    let uiColor: UIColor
+    
+}
+
